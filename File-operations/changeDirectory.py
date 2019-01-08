baaaaab8fa6fb683f9aa6585f877a869bd9ce461
Aditya Raj <aditya.x510@gmail.com>
2019-01-08 17:24:51 +0530

    change directory using import os

---
+++ File-operations/changeDirectory.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.getcwd()) # get current working directory
+os.chdir('E:\\MyWorkPlace\\Python-programming\\File-operations') #change directory where ypu want to save file
+print(os.listdir()) # list directory return the list of file in that directory
+
+
+#opening a existing file
