a82fe56df2281fec7da66984d51696e6a115882c
Aditya Raj <aditya.x510@gmail.com>
2019-01-08 16:13:22 +0530

    method overriding

--- overriding/methodOverriding.py
+++ overriding/methodOverriding.py
@@ -1,2 +1,23 @@
 # overriding of methods
+class PYCharm:
+    def execute(self):
+        print("Compiling")
+        print("Running")
 
+class VSCode:
+    def execute(self):
+        print("Compiling")
+        print("Running")
+        print("Text Editor")
+        print("Coding Conventions")
+
+class Laptop:
+    def code(self,ide):
+        ide.execute()
+
+ide1 = PYCharm()
+ide2 = VSCode()     
+
+lap1 = Laptop()
+# lap1.code(ide1)
+lap1.code(ide2)
