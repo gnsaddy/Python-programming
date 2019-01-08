346a6ece79bd0a0d1cabbd3482a89f40965e2372
Aditya Raj <aditya.x510@gmail.com>
2019-01-08 11:08:09 +0530

    Basic

--- abstractClasses/abstractClass.py
+++ abstractClasses/abstractClass.py
@@ -6,9 +6,27 @@ class parent(ABC):
     def show(self):
         pass
 
-class child(parent):
-    def show(self):
-        print("Implementation of abstract method in child class")
+    @abstractmethod
+    def disp(self):
+        pass
 
-obj = child()
+class child1(parent):
+    def show(self):
+        print("Implementation of abstract method in first child class")
+
+    def disp(self):
+        print("implementation in first child class")
+
+class child2(parent):
+    def show(self):
+        print("Show in second child class ")
+    def disp(self):
+        print("Display from class second ")
+
+obj = child1()
 obj.show()
+obj.disp()
+
+obj2 = child2()
+obj2.show()
+obj2.disp()
